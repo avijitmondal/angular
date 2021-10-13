c28444b2d5a4a63418ef2acc24504cda55c8fd34
Avijit <avijitmondal38@gmail.com>
2021-10-13 13:49:23 +0530

    Fixed typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiProxy: 'https://ec2-z3-87-65-113.compute-1.amazonaws.com',
+  apiProxy: 'https://ec2-3-87-65-113.compute-1.amazonaws.com',
   apiRest: 'https://ec2-3-87-65-113.compute-1.amazonaws.com',
   production: true
 };
